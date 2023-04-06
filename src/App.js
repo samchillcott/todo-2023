8db0b281c94245bc489b165fe5acbd3472027ff5
samchillcott1 <samchillcott@gmail.com>
2023-04-06 14:13:01 +0100

    restructure project to Login and TodoApp screens

--- src/App.js
+++ src/App.js
@@ -1,70 +1,22 @@
-import './App.css';
-import { useEffect, useState } from 'react';
-import Form from './components/Form';
-import TodoList from './components/TodoList';
+import React, { useState, useEffect } from 'react';
+import TodoApp from './screens/TodoApp';
+import Login from './screens/Login';
+import "./App.css"
 
-function App() {
+const App = () => {
+  const [authenticated, setAuthenticated] = useState(true);
 
-  const [inputText, setInputText] = useState("")
-  const [todos, setTodos] = useState([])
-  const [status, setStatus] = useState("all")
-  const [filteredTodos, setFilteredTodos] = useState([])
-
-  useEffect(() => {
-    getLocalTodos()
-  }, [])
-
-  useEffect(() => {
-    filterHandler()
-    saveLocalTodos()
-  }, [todos, status])
-
-  const filterHandler = () => {
-    switch (status) {
-      case 'complete':
-        setFilteredTodos(todos.filter(todo => todo.complete === true))
-        break
-      case 'incomplete':
-        setFilteredTodos(todos.filter(todo => todo.complete === false))
-        break
-      default:
-        setFilteredTodos(todos)
-        break
-    }
-  }
-
-  const saveLocalTodos = () => {
-    localStorage.setItem('todos', JSON.stringify(todos))
-  }
-
-  const getLocalTodos = () => {
-    if (localStorage.getItem('todos') === null) {
-      localStorage.setItem('todos', JSON.stringify([]))
-    } else {
-      let localTodos = JSON.parse(localStorage.getItem('todos'))
-      setTodos(localTodos)
-    }
-  }
+  // useEffect(() => {
+  //   const token = document.cookie.replace(/(?:(?:^|.*;\s*)token\s*=\s*([^;]*).*$)|^.*$/, '$1');
+  //   setAuthenticated(!!token);
+  // }, []);
 
   return (
-    <div className="App">
-      <header>
-        <h1>Todo List</h1>
-      </header>
-      <Form
-        inputText={ inputText }
-        setInputText={ setInputText }
-        todos={ todos }
-        setTodos={ setTodos }
-        setStatus={ setStatus }
-      />
-      <TodoList
-        todos={ todos }
-        setTodos={ setTodos }
-        filteredTodos={ filteredTodos }
-      />
-    </div>
+    <>
+      { !authenticated && <Login /> }
+      { authenticated && <TodoApp /> }
+    </>
   );
-}
+};
 
 export default App;
