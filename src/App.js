38099fe6d66affc8c2d1d811978693bc527f85e9
samchillcott1 <samchillcott@gmail.com>
2023-04-07 11:12:12 +0100

    reconfigure App with react router paths

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React, { useState, useEffect } from 'react';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import TodoApp from './screens/TodoApp';
 import Login from './screens/Login';
+import PrivateRoute from './PrivateRoute';
 import "./App.css"
 
 const App = () => {
-  const [authenticated, setAuthenticated] = useState(true);
-
-  useEffect(() => {
-    const token = document.cookie.replace(/(?:(?:^|.*;\s*)token\s*=\s*([^;]*).*$)|^.*$/, '$1');
-    setAuthenticated(!!token);
-  }, []);
-
   return (
-    <>
-      <header>
-        <h1>Todo List</h1>
-      </header>
-      { !authenticated && <Login setAuthenticated={setAuthenticated}/> }
-      { authenticated && <TodoApp setAuthenticated={ setAuthenticated } /> }
-    </>
+    <Router>
+      <Switch>
+        <Route path="/login" component={ Login } />
+        <PrivateRoute exact path="/" component={ TodoApp } />
+      </Switch>
+    </Router>
   );
 };
 
