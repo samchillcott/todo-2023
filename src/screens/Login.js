d2333e04606d28afe88dea92524be1e0bd381f66
samchillcott1 <samchillcott@gmail.com>
2023-04-07 13:47:49 +0100

    fix cookie setting to use email from response

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
 
-const Login = ({ setAuthenticated }) => {
+const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
@@ -20,7 +20,7 @@ const Login = ({ setAuthenticated }) => {
         },
       };
       // Set cookie and redirect to home page if login is successful
-      Cookies.set('user', { email });
+      Cookies.set('user', response.data.email );
       navigate('/');
     }, 1000);
   };
